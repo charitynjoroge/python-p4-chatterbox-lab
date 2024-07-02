9ee9e60a06359641c9f13c974ce0e7b889ba2967
Charity Njoroge <charity.njoroge@student.moringaschool.com>
2024-07-02 19:20:28 +0300

    update

--- server/app.py
+++ server/app.py
@@ -5,8 +5,8 @@ from flask_migrate import Migrate
 from models import db, Message
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 CORS(app)
@@ -14,13 +14,32 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route("/messages", methods=["GET", "POST"])
 def messages():
-    return ''
+    if request.method == "GET":
+        messages = Message.query.order_by("created_at").all()
+        return make_response(jsonify([message.to_dict() for message in messages]), 200)
 
-@app.route('/messages/<int:id>')
+    elif request.method == "POST":
+        data = request.get_json()
+        message = Message(body=data["body"], username=data["username"])
+        db.session.add(message)
+        db.session.commit()
+        return make_response(jsonify(message.to_dict()), 201)
+
+@app.route("/messages/<int:id>", methods=["PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+    if request.method == "PATCH":
+        data = request.get_json()
+        for attr, value in data.items():
+            setattr(message, attr, value)
+        db.session.commit()
+        return make_response(jsonify(message.to_dict()), 200)
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+        return make_response(jsonify({"deleted": True}), 200)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -9,6 +10,18 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 class Message(db.Model, SerializerMixin):
-    __tablename__ = 'messages'
-
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Message {self.id} - {self.username}: {self.body}>'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'body': self.body,
+            'username': self.username,
+            'created_at': self.created_at.isoformat()
+        }
